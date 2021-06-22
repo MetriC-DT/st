22c0c6831cf444f917bf5eab046e70cdfaec8e7c
Derick Tseng <derickboss1@gmail.com>
2021-06-22 00:40:56 -0700

    add a bit of border

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static char *font2[] = {
     "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true"
 };
 
-static int borderpx = 0;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
